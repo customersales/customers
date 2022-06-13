c7505c07d541cbc20d19ff958c672faeab352bea
customersales <107136248+customersales@users.noreply.github.com>
2022-06-13 21:09:30 +0530

    Update settings.py

--- customerSegmentation/settings.py
+++ customerSegmentation/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0*c0-9*1tqwlv061%ac!ut_y4+^&px_vm@*e*det5w=^p^^nwh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['customer-s.herokuapp.com']
 
 
